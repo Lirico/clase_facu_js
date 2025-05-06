cd0b4adfd79d40ae9cf1f88041263a3b48b7d148
lirico <dominguezmatiasadrian@gmail.com>
2025-05-06 13:37:56 -0300

    funciones

---
+++ Funciones/index.js
@@ -0,0 +1,21 @@
+/********* FUNCIONES ********/
+
+// SCOPE -> AMBITO/ALCANCE
+
+// SCOPE GLOBAL 
+
+// Funcion -> Siempre retorna un valor
+// Procedimiento -> No retorna nada -> Se encarga unicamente de ejecutar acciones
+
+
+
+function sumar(a, b){
+    // SCOPE LOCAL (scope del bloque de la funcion) 
+
+    return a + b
+}
+
+
+console.log(sumar(4, 5))
+console.log(sumar())
+
