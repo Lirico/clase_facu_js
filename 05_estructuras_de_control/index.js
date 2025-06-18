b5f21343e985c76b007f0201b9f35f93c714916c
lirico <dominguezmatiasadrian@gmail.com>
2025-06-18 16:33:13 -0300

    estrucutras de control de flujo

---
+++ 05_estructuras_de_control/index.js
@@ -0,0 +1,85 @@
+/********* ESTRUCTURAS DE CONTROL DE FLUJO ********/
+
+// if else
+// function mayorA10(num){
+//     if(num > 10){
+//         return "Es mayor a 10"
+//     } else if(num < 10) {
+//         return "Es menor que 10"
+//     } else {
+//         return "Es igual a 10"
+//     }
+// }
+
+// switch
+// function maquinaExpendedora(producto){ // numerico
+//     switch(producto){
+//         case 1: 
+//             return "Aguita"
+//         case 2:
+//             return "Cafe"
+//         case 3:
+//             return "Te"
+//         case 4: 
+//             return "Energizante"
+//         default:
+//             return "El valor ingresado no corresponde aun producto existente"
+//     }
+// }
+
+
+// ternario (ES2015)
+// function esPositivo(num){
+//     return num > 0 
+//         ? "Es positivo" 
+//         : num < 0
+//             ? "Es negativo"
+//             : "Es cero"
+// }
+
+// while (loop)
+// let contador = 0;
+
+// while(contador < 10){
+//     console.log(contador)
+//     contador++
+// }
+
+
+// do while (loop)
+// do {
+    
+// } while (condition);
+
+
+// for (loop)
+// for (let index = 0; index < array.length; index++) {
+    
+// }
+
+
+
+// forin (loop ES2015) (objetos)
+// const persona = {
+//     brazos: 2,
+//     piernas: 2,
+//     ojos: 2
+// }
+
+// for (const propiedad in persona) {
+//     console.log(persona[propiedad])
+// }
+
+// forof (loop ES2015) (arreglos)
+// const frutas = ["Mora", "Banana", "Manzana", "Pera", "Melon"]
+
+// for (const fruta of frutas) {
+//     console.log(fruta)
+// }
+
+
+// const tableroTaTeTi = [
+//     ["","",""],
+//     ["","O",""],
+//     ["","","X"]
+// ]   
