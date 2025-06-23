0a10ddcbea59359a5c50c5be0045a8480242028d
lirico <dominguezmatiasadrian@gmail.com>
2025-06-23 11:58:46 -0300

    02 ejercicios

---
+++ 02_ejercicios/index.js
@@ -0,0 +1,103 @@
+/*
+    Ejercicio 1:
+
+    Programar una funcion que tas recibir un numero retorne "Es par" si el numero
+    es par. De lo contrario debe retornar "Es impar"
+*/
+function esPar(numero){
+    // tu codigo
+}
+
+/*
+    Ejercicio 2:
+
+    Programar una funcion que  tras recibir dos valores "a" y "b" retorne el mayor
+    de ambos.
+*/
+function mayorDeDos(a,b){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 3:
+
+    Programar una funcion que tras recibir una nota retorne una calificacion en letra
+    equivalente a dicha nota.
+*/
+function calificacionEnLetras(nota){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 4:
+
+    Programar una funcion que tras recibir un valor "n" imprima todos los numeros
+    desde 1 hasta "n".
+*/
+function contarHastaN(n){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 5:
+
+    Programar una funcion que tras recibir un valor "n" sume todos los numeros 
+    desde 1 hasta "n" y retorne el total.
+*/
+function sumaPrimerosN(n){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 6:
+
+    Programar una funcion que tras recibir un valor "numero" retorne la tabla
+    de multiplicar de dicho numero en formad e una cadena de texto.
+*/
+function tablaDeMultiplicar(numero){
+    // Tu codigo
+}
+/*
+    Ejercicio 7:
+
+    Programar una funcion que tras recibir un valor "num" retorne "Positivo"
+    si el numero es positivo, "Negativo" si es negativo y "Cero" si es cero.
+    NOTA: Resolver con ternario.
+*/
+function tipoNumero(num){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 8:
+
+    Programar una funcion que tras recibir un valor "numero", y teniendo en cuenta
+    que cada dia de la semana tiene un valor numerico implicito (ej: 1 domingo, 2 lunes, etc)
+    retorne "Es dia de la semana" si es un dia habil. De lo contrario debe retornar
+    "Es fin de semana". 
+*/
+function diaDeLaSemana(numero){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 9:
+
+    Programar una funcion que tras recibir los valores "a", "b" y "c" como lados de un triangulo, 
+    compare dichos valores y retorne "Equilatero", "Isoceles" o "Escaleno".
+*/
+function tipoTriangulo(a, b, c){
+    // Tu codigo
+}
+
+/*
+    Ejercicio 10:
+
+    Programar una funcion que tras recibir un valor en grados "celsius" retorne 
+    su equivalente en Fahrenheit. Si "celsius" es un tipo de dato no numerico
+    debe retornar "Valor no valido". 
+    Nota: Se debe usar un ternario.
+*/
+function celsiusAFahrenheit(celsius){
+    // Tu codigo
+}
